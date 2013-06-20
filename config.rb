3df1d7aa18ec83586db3700b91ebf54ac94f7134
Ivana Setiawan <ivana.setiawan@gmail.com>
2013-06-20 16:46:51 +0200

    New design

--- config.rb
+++ config.rb
@@ -3,13 +3,13 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 # Path for images
-http_images_path = "/assets/images/"
-http_generated_images_path = "/assets/images/"
+http_images_path = "/images/"
+http_generated_images_path = "/images/"
 
-css_dir = "assets/stylesheets"
-sass_dir = "assets/stylesheets/sass"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
+css_dir = "public/css"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/js"
 
 output_style = :compressed
 
